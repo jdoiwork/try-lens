22d3dfb25dbfb7a1621bfd3e651143c47f9c3b94
jdoi <jdoi.work@gmail.com>
2019-02-02 02:06:36 +0900

    単純な使い方

--- src/Lib.hs
+++ src/Lib.hs
@@ -1,6 +1,27 @@
+{-# LANGUAGE TemplateHaskell #-}
+
 module Lib
-    ( someFunc
-    ) where
+    where
+
+import Control.Lens
 
 someFunc :: IO ()
 someFunc = putStrLn "someFunc"
+
+data Address = Address
+             { _name :: String
+             , _email :: String
+             } deriving (Show)
+
+makeLenses ''Address
+
+
+addr :: Address
+addr = Address "jdoi" "jdoi@example.com"
+
+getAddrName :: String
+getAddrName = addr ^. name
+
+setAddrName :: String -> Address
+setAddrName newName = addr & name .~ newName
+
