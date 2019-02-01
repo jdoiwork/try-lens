0c9ee2e73ea60f9cc9f239e1d030db119757101f
jdoi <jdoi.work@gmail.com>
2019-02-02 02:39:57 +0900

    同じフィールド名でもできるようにした

--- src/Lib.hs
+++ src/Lib.hs
@@ -9,19 +9,38 @@ someFunc :: IO ()
 someFunc = putStrLn "someFunc"
 
 data Address = Address
-             { _name :: String
-             , _email :: String
+             { addressName :: String
+             , addressEmail :: String
+             , addressSub :: SubAddress
              } deriving (Show)
 
-makeLenses ''Address
+data Address2 = Address2
+              { address2Name :: String
+              , address2Email :: String
+              } deriving (Show)
 
+data SubAddress = SubAddress
+                { subAddressName :: String
+                , subAddressEmail :: String
+                } deriving (Show)
+
+              
+--makeLenses ''Address
+makeFields ''Address
+makeFields ''Address2
+makeFields ''SubAddress
 
 addr :: Address
-addr = Address "jdoi" "jdoi@example.com"
+addr = Address "jdoi" "jdoi@example.com" $ SubAddress "aaa" "aaa@example.com"
 
 getAddrName :: String
 getAddrName = addr ^. name
 
+getSubAddrName :: String
+getSubAddrName = addr ^. sub . name
+
 setAddrName :: String -> Address
 setAddrName newName = addr & name .~ newName
 
+setSubAddrName :: String -> Address
+setSubAddrName newName = addr & sub . name .~ newName
